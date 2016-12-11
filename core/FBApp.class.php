df138a1474a7f947f2c9249e8b31cdc0b833673d
Charles <cnerot91@gmail.com>
2016-12-11 12:47:52 +0100

    Added fb upload function

--- core/FBApp.class.php
+++ core/FBApp.class.php
@@ -19,19 +19,19 @@ class FBApp
         ]);
         $this->loginhelper = $this->fb->getRedirectLoginHelper();
 
-        if (isset($_SESSION['facebook_access_token'])){
+        if (isset($_SESSION['facebook_access_token'])) {
             $this->fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
         }
     }
 
     public function printFBLogin($callback, $permissions = [], $login_text = 'Log in with Facebook!', $logged_text = 'Logged in with FB!!')
     {
-        if (!isset($_SESSION['facebook_access_token'])){
+        if (!isset($_SESSION['facebook_access_token'])) {
             $this->loginhelper = $this->fb->getRedirectLoginHelper();
             $loginUrl = $this->loginhelper->getLoginUrl($callback, $permissions);
             echo '<a href="' . $loginUrl . '">' . $login_text . '</a>';
         } else {
-            echo '<p>' .$logged_text. '</p>';
+            echo '<p>' . $logged_text . '</p>';
         }
     }
 
@@ -46,7 +46,7 @@ class FBApp
             Logger::logExeption($e);
             return false;
         }
-        if (! isset($accessToken)) {
+        if (!isset($accessToken)) {
             if ($this->loginhelper->getError()) {
                 header('HTTP/1.0 401 Unauthorized');
                 $error = "Error: " . $this->loginhelper->getError() . "\n";
@@ -71,6 +71,37 @@ class FBApp
     public function getFBData($fb_query)
     {
         return $this->fb->get($fb_query);
+    }
+
+    //TODO:: test upload
+    public function upload($file, $fbpath, $name, $message = "Uploaded with fournine")
+    {
+        $batch = [
+            $name => $this->fb->request('POST', $fbpath, [
+                'message' => $message,
+                'source' => $this->fb->fileToUpload('/path/to/photo-one.jpg'),
+            ])
+        ];
+        try {
+            $responses = $this->fb->sendBatchRequest($batch);
+        } catch (Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            Logger::logExeption($e);
+            exit;
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            Logger::logExeption($e);
+            exit;
+        }
+        $return_status = true;
+        foreach ($responses as $key => $response) {
+            if ($response->isError()) {
+                $e = $response->getThrownException();
+                Logger::logExeption($e);
+                $return_status = true;
+            }
+        }
+        return $return_status;
     }
 
 }
