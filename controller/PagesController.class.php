e8821b58c738be50d8840e0cbf4ade3f918c6c11
Charles <cnerot91@gmail.com>
2017-01-06 14:38:20 +0100

    Added pages integration & rename to pages

---
+++ controller/PagesController.class.php
@@ -0,0 +1,78 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class AdminController
+{
+    public $form;
+
+    public function preDeploy($args)
+    {
+        $this->form = new Form([
+            'options' => [
+                'method' => 'POST',
+                'action' => '#',
+                'submit' => 'Send',
+                'name' => 'postform',
+                'class' => '',
+                'enctype' => "multipart/form-data"
+            ],
+            'data' => [
+                "title" => [
+                    "type" => "text",
+                    "validation" => "text",
+                    "value" => ''
+                ],
+                "content" => [
+                    "type" => "textarea",
+                    "validation" => "text",
+                    "value" => ''
+                ],
+            ]
+        ]);
+    }
+
+    /**
+     * Static pages main
+     */
+    public function indexAction($args)
+    {
+        $page = new Staticpages();
+        $data = $this->form->validate();
+        if ($data){
+            if (isset($_POST['seperator'])){
+                $pages = $page->getWhere(['id'=>$_POST['seperator']]);
+                $data['id'] = $_POST['seperator'];
+            }
+            $page->fromArray($data);
+            $page->save();
+        }
+        $pages = $page->getWhere([]);
+
+        $view = new View();
+        $view->setView('staticMenu');
+        $view->putData('pages', $pages);
+        $view->putData('form', $this->form);
+
+    }
+
+    /**
+     * Needed for clonable element
+     */
+    public function ajaxAction($args)
+    {
+        $data = $this->form->validate();
+        if ($data){
+            $page = new Staticpages();
+            $page->fromArray($data);
+            $page->save();
+        }
+        $view = new View();
+        $view->setView('staticMenu/clonable', 'no_layout');
+        $view->putData('form', $this->form);
+    }
+}
--- view/staticMenu.php
+++ view/staticMenu.php
@@ -1,24 +1,18 @@
-   <div class="container">
-        <div class="row">
-            <div class="col s12 p_pageStat">
-                <p>Création des pages statiques</p>
-            </div>
-            <div class="col s12 margin-top">
-                <a class="btn-floating btn-large green lighten-1 right"><i class="large material-icons">add</i></a>
-            </div>
-            <div class="col s12">
-                <ul class="collapsible popout" data-collapsible="accordion">
-                    <li>
-                      <div class="collapsible-header yellow"><i class="material-icons">description</i>page 3</div>
-                      <div class="collapsible-body yellow">
-                          <div class="container">
-                              <br>
-                            <input type="text" id="input_text_stat" class="" placeholder="Titre de la page">
-                            <textarea class="textarea_stat" rows="10" id="comment" placeholder="Contenur de la page"></textarea>
-                          </div>
-                      </div>
-                    </li>
-                </ul>
-            </div>
-        </div>         
+<div class="container">
+    <div class="row">
+        <div class="col s12 p_pageStat">
+            <p>Création des pages statiques</p>
+        </div>
+        <div class="col s12 margin-top">
+            <a class="btn-floating btn-large green lighten-1 right" id='static-container-button'><i class="large material-icons">add</i></a>
+        </div>
+        <div class="col s12">
+            <ul class="collapsible popout" id='static-container' data-collapsible="accordion">
+                <?php foreach ($pages as $page) {
+                    $this->getChild('clonable', ['page' => $page, 'form' => $form]);
+                } ?>
+
+            </ul>
+        </div>
     </div>
+</div>
