bddecb855295113d51449b105f58c47f6d8e99da
Charles <cnerot91@gmail.com>
2017-01-06 14:38:38 +0100

    added pages js

--- media/js/script.js
+++ media/js/script.js
@@ -1,27 +1,34 @@
-$(document).ready(function(){
-	showHideGift("hide");
-	
-	$("#nameGift").change(function(){ // quand photo sélectionnée
-		var nameGift = $("#nameGift").val();
-	
-		$("#infoNameGift").html(nameGift);
-		
-		showHideGift("show");
-	});
-	
-	$(".cancelImageGift").click(function(){ // clic annuler image lot
-		$("#nameGift").val("");
-		
-		showHideGift("hide");
-	});
-	
-	function showHideGift(showHide){
-		if(showHide == "show"){
-			$(".containerImageGift").show();
-			$(".cancelImageGift").show();
-		}else{
-			$(".containerImageGift").hide();
-			$(".cancelImageGift").hide();
-		}
-	}
+$(document).ready(function () {
+    showHideGift("hide");
+
+    $("#nameGift").change(function () { // quand photo sélectionnée
+        var nameGift = $("#nameGift").val();
+
+        $("#infoNameGift").html(nameGift);
+
+        showHideGift("show");
+    });
+
+    $(".cancelImageGift").click(function () { // clic annuler image lot
+        $("#nameGift").val("");
+
+        showHideGift("hide");
+    });
+
+    function showHideGift(showHide) {
+        if (showHide == "show") {
+            $(".containerImageGift").show();
+            $(".cancelImageGift").show();
+        } else {
+            $(".containerImageGift").hide();
+            $(".cancelImageGift").hide();
+        }
+    }
+
+    $("#static-container-button").click(function () {
+        $.ajax({url: "http://four-nine.local.fr/admin/ajax", success: function(result){
+             $("#static-container").append(result);
+        }});
+    });
+
 });
