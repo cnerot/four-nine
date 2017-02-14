08906c13d1b1c8eb3a669400617e896cbf343f29
gaetan.ab <gaetan.ab@gmail.com>
2017-02-14 09:58:03 +0100

    récupération concours en cours,  affichage des photos du concours

--- view/voteConcours.php
+++ view/voteConcours.php
@@ -1,27 +1,20 @@
   <div class="container">
     <div class="row">
-	<h1>photggos participants au concours</h1>
+	<h1>Photos des participants au concours <?php echo $contestCurrent->name; ?> 
+	<br><span style="font-size: 20px;">(concours valable du <?php echo $contestCurrent->start; ?> au <?php echo $contestCurrent->end; ?>)</span></h1>
         <div id="imageGallery">
             <div class="col s3 ">
-                <a href="http://img0.gtsstatic.com/tatouage/un-tatouage-d-inspiration-steam-punk_165510_w250.jpg">
-                    <img src="http://img0.gtsstatic.com/tatouage/un-tatouage-d-inspiration-steam-punk_165510_w250.jpg" width="100" alt="nom prenom" title="nom prenom">
-                </a>
+				<?php foreach($listPhotos as $photo) : ?>
+					<a id="<?php echo $photo->infosPhotoFb['id']; ?>" href="<?php echo $photo->infosPhotoFb['source']; ?>">
+						<img src="<?php echo $photo->infosPhotoFb['source']; ?>" width="100" alt="photo" title="nom prenom">
+					</a>
+				<?php endforeach; ?>               
                 <?php
                 if (isset($_POST['star'])){
                     var_dump($_POST['star']);
                 }
                 ?>
             </div>
-            <div class="col s3 ">
-                <a href="http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg">
-                    <img src="http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg" width="100" alt="nom2 prenom" title="nom2 prenom">
-                </a>
-                <?php
-                if (isset($_POST['star'])){
-                     var_dump($_POST['star']);
-                }
-                ?>
-        </div>
      </div>
 </div>
   </div>
