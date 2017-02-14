fdd9f05ac0ff5c9af594c7308d0d7416ed477b3b
gaetan.ab <gaetan.ab@gmail.com>
2017-02-14 11:48:10 +0100

    pagination galerie d'images

--- view/voteConcours.php
+++ view/voteConcours.php
@@ -3,18 +3,25 @@
 	<h1>Photos des participants au concours <?php echo $contestCurrent->name; ?> 
 	<br><span style="font-size: 20px;">(concours valable du <?php echo $contestCurrent->start; ?> au <?php echo $contestCurrent->end; ?>)</span></h1>
         <div id="imageGallery">
-            <div class="col s3 ">
-				<?php foreach($listPhotos as $photo) : ?>
-					<a id="<?php echo $photo->infosPhotoFb['id']; ?>" href="<?php echo $photo->infosPhotoFb['source']; ?>">
-						<img src="<?php echo $photo->infosPhotoFb['source']; ?>" width="100" alt="photo" title="nom prenom">
-					</a>
-				<?php endforeach; ?>               
-                <?php
-                if (isset($_POST['star'])){
-                    var_dump($_POST['star']);
-                }
-                ?>
-            </div>
+			<?php for($i = 0; $i<count($listPhotos); $i++) : ?>
+				<div class="col s3 <?php echo $i+1; ?> <?php if($i>=8) echo "hidden"; ?>">
+					<?php foreach($listPhotos as $photo) : ?>
+						<a id="<?php echo $photo->infosPhotoFb['id']; ?>" href="<?php echo $photo->infosPhotoFb['source']; ?>">
+							<img src="<?php echo $photo->infosPhotoFb['source']; ?>" width="100" alt="photo" title="nom prenom">
+						</a>
+					<?php endforeach; ?>               
+					<?php
+					if (isset($_POST['star'])){
+						var_dump($_POST['star']);
+					}
+					?>
+				</div>
+			<?php endfor; ?>
+			
+			<div class="col s12">
+				<input type="hidden" id="nbPhotosToDisp" value="8">
+				<input type="button" id="seeMorePhotos" value="Voir +" class="btn">
+			</div>
      </div>
 </div>
   </div>
